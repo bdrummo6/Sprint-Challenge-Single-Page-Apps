98532859c9810264416be6f90b2d7d69b11faddd
bdrummo6 <bradley.drummonds@gmail.com>
2020-03-22 20:18:48 -0400

    Rendered List of Characters
    Rendered list of the characters with three attributes.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+export default function CharacterList(props) {
+   const [characters, setCharacters] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+   useEffect(() => {
+      axios.get(`https://rickandmortyapi.com/api/character/`)
+         .then(response => {
+            // console.log(response.data);
+            setCharacters(response.data.results);
+         })
+         .catch(error => {
+            console.log(error);
+         })
+   }, []);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+   return (
+      <section className="character-list">
+         <div className='characters'>
+            <h2>List of Characters</h2>
+            {characters.map(character => {
+               return (
+                  <div className='character' key={character.id}>
+                     <h3>{character.name}</h3>
+                     <div>
+                        <p>Status: {character.status}</p>
+                        <p>Gender: {character.gender}</p>
+                        <p>Species: {character.species}</p>
+                     </div>
+                  </div>
+               )
+            })}
+         </div>
+      </section>
+   );
 }
+
