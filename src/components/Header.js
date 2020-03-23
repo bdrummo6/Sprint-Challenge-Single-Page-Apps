2ffdbf715c912b327f04cc3a2204481b11ff22cb
bdrummo6 <bradley.drummonds@gmail.com>
2020-03-22 20:19:52 -0400

    Created two Links in the Header
    These links are for the Welcome Page and the list of characters.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
 export default function Header() {
-  return (
+  const Buttons = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    width: 30%;
+    margin: 10px 32% 10px 32%;
+  `;
+
+   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Buttons>
+         <button><Link to='/'>Home</Link></button>
+         <button><Link to='/characters'>Characters</Link></button>
+      </Buttons>
     </header>
   );
 }
